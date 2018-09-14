ba3d06c27315c86832a040e48c38d2828718ad86
Jan-Erik Rediger <janerik@fnordig.de>
2018-09-14 20:18:56 +0200

    Copy existing mdbook commands

---
+++ src/cmd/mod.rs
@@ -0,0 +1,7 @@
+//! Subcommand modules for the `mdbook` binary.
+
+pub mod build;
+pub mod clean;
+pub mod init;
+pub mod serve;
+pub mod watch;
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,91 @@
+extern crate chrono;
+#[macro_use]
+extern crate clap;
 extern crate env_logger;
+#[macro_use]
+extern crate log;
 extern crate mdbook;
 extern crate mdbook_mermaid;
 extern crate mdbook_toc;
-extern crate clap;
+extern crate open;
 
-use mdbook::MDBook;
-use mdbook::errors::Result;
-use mdbook_mermaid::Mermaid;
-use mdbook_toc::Toc;
-use clap::{App, ArgMatches};
-
+use chrono::Local;
+use clap::{App, AppSettings, ArgMatches};
+use env_logger::Builder;
+use log::LevelFilter;
+use mdbook::utils;
 use std::env;
-use std::process;
+use std::ffi::OsStr;
+use std::io::Write;
 use std::path::{Path, PathBuf};
+
+mod cmd;
+
+const NAME: &'static str = "mdbook-dtmo";
+const VERSION: &'static str = concat!("v", crate_version!());
+
+fn main() {
+    init_logger();
+    //
+    // Create a list of valid arguments and sub-commands
+    let app = App::new(NAME)
+        .about("Creates a book from markdown files with added plugins")
+        .author("Jan-Erik Rediger <jrediger@mozilla.com>")
+        .version(VERSION)
+        .setting(AppSettings::GlobalVersion)
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .after_help(
+            "For more information about a specific command, try `mdbook-dtmo <command> --help`\n\
+             The source code for mdBook is available at: https://github.com/badboy/mdbook-dtmo",
+        )
+        .subcommand(cmd::init::make_subcommand())
+        .subcommand(cmd::build::make_subcommand())
+        .subcommand(cmd::clean::make_subcommand())
+        .subcommand(cmd::watch::make_subcommand())
+        .subcommand(cmd::serve::make_subcommand());
+
+    // Check which subcomamnd the user ran...
+    let res = match app.get_matches().subcommand() {
+        ("init", Some(sub_matches)) => cmd::init::execute(sub_matches),
+        ("build", Some(sub_matches)) => cmd::build::execute(sub_matches),
+        ("clean", Some(sub_matches)) => cmd::clean::execute(sub_matches),
+        ("watch", Some(sub_matches)) => cmd::watch::execute(sub_matches),
+        ("serve", Some(sub_matches)) => cmd::serve::execute(sub_matches),
+        (_, _) => unreachable!(),
+    };
+
+    if let Err(e) = res {
+        utils::log_backtrace(&e);
+
+        ::std::process::exit(101);
+    }
+}
+
+fn init_logger() {
+    let mut builder = Builder::new();
+
+    builder.format(|formatter, record| {
+        writeln!(
+            formatter,
+            "{} [{}] ({}): {}",
+            Local::now().format("%Y-%m-%d %H:%M:%S"),
+            record.level(),
+            record.target(),
+            record.args()
+        )
+    });
+
+    if let Ok(var) = env::var("RUST_LOG") {
+        builder.parse(&var);
+    } else {
+        // if no RUST_LOG provided, default to logging at the Info level
+        builder.filter(None, LevelFilter::Info);
+        // Filter extraneous html5ever not-implemented messages
+        builder.filter(Some("html5ever"), LevelFilter::Error);
+    }
+
+    builder.init();
+}
 
 fn get_book_dir(args: &ArgMatches) -> PathBuf {
     if let Some(dir) = args.value_of("dir") {
@@ -28,6 +101,12 @@ fn get_book_dir(args: &ArgMatches) -> PathBuf {
     }
 }
 
+fn open<P: AsRef<OsStr>>(path: P) {
+    if let Err(e) = open::that(path) {
+        error!("Error opening web browser: {}", e);
+    }
+}
+
 pub fn make_app<'a, 'b>() -> App<'a, 'b> {
     App::new("mdbook-dtmo")
         .about("Build the book from the markdown files for dtmo")
@@ -38,30 +117,4 @@ pub fn make_app<'a, 'b>() -> App<'a, 'b> {
         .arg_from_usage(
             "[dir] 'A directory for your book{n}(Defaults to Current Directory when omitted)'",
         )
-}
-
-pub fn execute(args: &ArgMatches) -> Result<()> {
-    let book_dir = get_book_dir(args);
-    let mut book = MDBook::load(&book_dir)?;
-
-    if let Some(dest_dir) = args.value_of("dest-dir") {
-        book.config.build.build_dir = PathBuf::from(dest_dir);
-    }
-
-    book.with_preprecessor(Mermaid);
-    book.with_preprecessor(Toc);
-    book.build()?;
-
-    Ok(())
-}
-
-fn main() {
-    env_logger::init();
-    let app = make_app();
-    let matches = app.get_matches();
-
-    if let Err(e) = execute(&matches) {
-        eprintln!("{}", e);
-        process::exit(1);
-    }
 }
